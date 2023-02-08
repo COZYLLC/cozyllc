14f54b309c02de6dd75ecdf0bd575061ea7777e5
Choi Kyoung Min <ckm0728wash@gmail.com>
2023-02-08 13:21:20 +0900

    FIX: Implement template syntax

--- src/components/Footer/Info.vue
+++ src/components/Footer/Info.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <p>{info.name}</p>
-    <p>사업자등록번호 : {info.corpno}</p>
-    <p>{info.address}</p>
-    <p>전화문의 : {info.tel}</p>
-    <p>이메일 : {info.email}</p>
+    <p>{{info.name}}</p>
+    <p>사업자등록번호 : {{info.corpno}}</p>
+    <p>{{info.address}}</p>
+    <p>전화문의 : {{info.tel}}</p>
+    <p>이메일 : {{info.email}}</p>
   </div>
 </template>
 
